a09899d9b5b8336709d3e6672bf357cb728cc384
danielvallance <danielvallance3435@gmail.com>
2024-11-28 16:46:08 +0000

    Basic logging of message
    Added text field and button widget. Once button
    is pressed, text field contents are logged (along
    with its base64 encoding).
    
    The base64 encoding will be sent over bluetooth
    when this button is pressed in the final implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,13 +17,21 @@ class BluetoothTransceiverApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BluetoothTransceiverHomePage(),
+      home: BluetoothTransceiverHomePage(),
     );
   }
 }
 
 class BluetoothTransceiverHomePage extends StatelessWidget {
-  const BluetoothTransceiverHomePage({super.key});
+  BluetoothTransceiverHomePage({super.key});
+
+  final TextEditingController _controller = TextEditingController();
+
+  void _handlePress() {
+    String message = _controller.text;
+    String base64message = base64.encode(message.codeUnits);
+    log('Text entered: $message base64 encoding: $base64message');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +40,21 @@ class BluetoothTransceiverHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Bluetooth Transceiver Home Page"),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'TODO: Add bluetooth transceiver UI widgets',
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter text to send over Bluetooth',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _handlePress,
+              child: const Text('Send'),
             ),
           ],
         ),
